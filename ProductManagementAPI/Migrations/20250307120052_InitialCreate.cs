a01a31ef50fc8dd2014b48e48c149eb973f55528
Pham Thang <winnieph13@gmail.com>
2025-03-07 21:15:55 +0700

    update

---
+++ ProductManagementAPI/Migrations/20250307120052_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "hang_hoa",
+                columns: table => new
+                {
+                    ma_hanghoa = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    ten_hanghoa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    so_luong = table.Column<int>(type: "int", nullable: false),
+                    ghi_chu = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_hang_hoa", x => x.ma_hanghoa);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "hang_hoa");
+        }
+    }
+}
--- ProductManagementAPI/Program.cs
+++ ProductManagementAPI/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ProductDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlServerOptionsAction => sqlServerOptionsAction.EnableRetryOnFailure()));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
